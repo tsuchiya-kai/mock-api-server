732cc1a8835d70c603e67c80564a6e14e165ff41
KaitaTsuchiya <yukaigangan@gmail.com>
2023-04-26 21:15:11 +0900

    cors問題を解決

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SampleController } from './sample/sample.controller';
+import * as cors from 'cors';
 
 @Module({
   imports: [],
   controllers: [AppController, SampleController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cors()).forRoutes('*');
+  }
+}

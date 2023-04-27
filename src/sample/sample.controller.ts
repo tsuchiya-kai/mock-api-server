6dba413dc7c84942a1ea039857eb2fb8d07a30b8
KaitaTsuchiya <yukaigangan@gmail.com>
2023-04-27 12:38:12 +0900

    postメソッドの検証用APIを追加

--- src/sample/sample.controller.ts
+++ src/sample/sample.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 @Controller('api')
 export class SampleController {
+  private isObject(value: any): boolean {
+    return value !== null && typeof value === 'object';
+  }
+
   @Get('sample')
   getSample(): Record<string, unknown> {
     return {
       message: 'Hello, this is a mock API!',
+    };
+  }
+
+  // post用
+  @Post('sample')
+  postSample(@Body() data: any) {
+    console.log({ data });
+    return {
+      status: 200,
+      data,
     };
   }
 }

236ef52480c9ed816ccda51e3112e015d8441794
KaitaTsuchiya <yukaigangan@gmail.com>
2023-06-08 13:52:04 +0900

    モジュールを追加

--- src/retry/retry.controller.ts
+++ src/retry/retry.controller.ts
@@ -3,6 +3,7 @@ import {
   Req,
   Get,
   Query,
+  Body,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
@@ -13,9 +14,10 @@ export class RetryController {
   @Get('/')
   getSample(
     @Req() req: Request,
-    @Query() { isRetry }: { isRetry: boolean },
+    @Query() { isRetry }: { isRetry: 'true' | 'false' },
   ): Record<string, unknown> | HttpException {
-    if (isRetry) {
+    console.log({ isRetry });
+    if (isRetry === 'true') {
       const {
         method,
         query,
@@ -32,6 +34,21 @@ export class RetryController {
     // gateway error
     throw new HttpException(
       { error: { type: 'invalid_session', message: '無効なセッションです' } },
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+
+  @Get('/apis/init')
+  init(@Body() { isSuccess = 'true' }: { isSuccess: 'true' | 'false' }) {
+    // success
+    if (isSuccess === 'true') return {};
+
+    //error
+    throw new HttpException(
+      {
+        error: { type: 'already_initialized', message: '無効なセッションです' },
+      },
+      // { error: { type: 'no_session_id', message: '無効なセッションです' } },
       HttpStatus.BAD_REQUEST,
     );
   }
